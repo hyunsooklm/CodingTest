447176e0aaa05c1486da7472fcb466f57afea990
hyunsooklm <hyunsooklm@naver.com>
2022-04-20 23:02:47 +0900

    bruteforce+bfs풀이

--- main.py
+++ main.py
@@ -1,49 +1,55 @@
+import copy
+from itertools import combinations
 from collections import deque
-visit=set([])
-def prunning(history):
-    prun=False
-    if history=='0':
-        return prun
-    for v in visit:
-        #0145 015 02 0345 035 045 -> 015 in 0145, 035 in 0345 045 in 0145 or 045 in  0345
-        n=0
-        for s in history:
-            if s not in v:
-                break
-            else:
-                n+=1
-        if n==len(history):
-            prun=True
-            break
-    return prun
+VIRUS=2
+WALL=1
+EMPTY=0
+dx=[-1,0,1,0]
+dy=[0,1,0,-1]
 
-def dfs(N,T_list,P_list):
-    stack=deque([])
-    maxnum=0
-    stack.append([0,0,'0'])
-    while stack:
-        day,total,history=stack.pop()
-        print(f'day:{day} history:{history},maxnum:{maxnum}')
-        if prunning(history):
-            continue
-        visit.add(history)  # 뻗어나갈 준비가 되있는 상태
-        if day+T_list[day]<N+1:
-            for n in range(day+T_list[day],N+1):
-                if n+T_list[n]-1<=N:#해당일에 근무할 수 있다면
-                    subsum=total+P_list[n]
-                    new_history=history+str(n)
-                    maxnum = max(maxnum, subsum)
-                    print(f'subsum:{subsum},new_history:{new_history}')
-                    stack.append([n,subsum,new_history])
-    return maxnum
+def bfs(board):
+    answer=0
+    queue=deque([])
+    for r in range(N):
+        for c in range(M):
+            if board[r][c]==VIRUS:
+                queue.append([r,c])
+    while queue:
+        r,c=queue.popleft()
+        for i in range(4):
+            nr=r+dx[i]
+            nc=c+dy[i]
+            if 0<=nr<N and 0<=nc<M and board[nr][nc]==EMPTY:
+                board[nr][nc]=VIRUS
+                queue.append([nr,nc])
+
+    for r in range(N):
+        for j in range(M):
+            if board[r][j]==EMPTY:
+                answer+=1
+    return answer
 if __name__=='__main__':
-    N=int(input())
-    T_list=[1]  #0도 있으니까
-    P_list=[0]
-    for _ in range(N):
-        T,P=map(int,input().split())
-        T_list.append(T)
-        P_list.append(P)
-    print(f'T_list:{T_list}, P_list:{P_list}')
-    answer=dfs(N,T_list,P_list)
+    answer=0
+    N,M=map(int,input().split())
+    board=[]
+    virus_lst=deque([])
+    empty_lst=deque([])
+    for i in range(N):
+        board.append(list(map(int,input().split())))
+    for r in range(N):
+        for c in range(M):
+            if board[r][c]==EMPTY:
+                empty_lst.append([r,c])
+    candidate_lst=list(combinations(empty_lst,3))
+    for a,b,c in candidate_lst:
+        a_l,a_r=a
+        b_l,b_r=b
+        c_l,c_r=c
+        sub_board=copy.deepcopy(board)
+        sub_board[a_l][a_r] = WALL
+        sub_board[b_l][b_r] = WALL
+        sub_board[c_l][c_r] = WALL
+        numwall=bfs(sub_board)
+        answer=max(answer,numwall)
+    print(len(candidate_lst))
     print(answer)
